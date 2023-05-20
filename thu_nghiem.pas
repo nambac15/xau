5203a3ba2e5689b376663d0cb1ddfb6c4825cfda
Nam Bac <133018030+nambac15@users.noreply.github.com>
2023-05-21 06:28:17 +0700

    chuongtrinhcon

---
+++ thu_nghiem.pas
@@ -0,0 +1,27 @@
+var a:array[1..10000] of integer;
+    i,n:integer;
+    nhat:word;
+    procedure findMax( m: integer;b:array of integer;var max:word);
+var
+  k: integer;
+begin
+   
+        max := b[1];
+        for k:= 2 to m do
+        begin
+            if b[k] > max then max:=b[k];  
+    end;
+end;
+begin
+    read(n);
+    for i:=1 to n do 
+    readln(a[i]);
+    
+    
+    findMax(n,a[i],nhat);
+    
+    write('so lon nhat : ',nhat);
+
+    readln;
+end.
+
